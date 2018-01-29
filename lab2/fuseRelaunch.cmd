40b0629052a0112a63f4f53fa02b7ac222bd39da
MarshalLeeeeee <790920655@qq.com>
2018-01-29 10:12:20 +0800

    upload lab2 on 20180129

---
+++ lab2/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "F:/document/SJTU/course/computer_architecture/experiment/workshop/lmc/lab2/counter/countert_isim_beh.exe" -prj "F:/document/SJTU/course/computer_architecture/experiment/workshop/lmc/lab2/counter/countert_beh.prj" "work.countert" "work.glbl" 
